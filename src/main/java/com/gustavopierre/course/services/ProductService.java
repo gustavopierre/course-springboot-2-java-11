e87d56d65a8e980658f774f5a943aa3777b3646a
Gustavo Pierre <gustavopierre@gmail.com>
2022-06-06 08:35:10 +0100

    Product

---
+++ src/main/java/com/gustavopierre/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.gustavopierre.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gustavopierre.course.entities.Product;
+import com.gustavopierre.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
